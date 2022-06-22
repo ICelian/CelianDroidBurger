f8420a47bbe50b347997073cf9b261efb4135258
xx_my <lopezchefchoufi.celian@gmail.com>
2022-06-23 01:27:39 +0200

    devfinalpart

--- app/src/main/java/fr/isen/lopez/celiandroidburger/ConfirmationActivity.kt
+++ app/src/main/java/fr/isen/lopez/celiandroidburger/ConfirmationActivity.kt
@@ -1,13 +1,39 @@
 package fr.isen.lopez.celiandroidburger
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ConfirmationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmation)
+
+        val sharedPref = this.getSharedPreferences(getString(R.string.sharedpref), Context.MODE_PRIVATE)
+
+        val getname = sharedPref.getString("saved_name", "default_name")
+        val getfirstname = sharedPref.getString("saved_name", "default_name")
+        val getadresse = intent.getStringExtra("referAddress")
+        val getnumero = intent.getStringExtra("referPhone")
+        val getburger = intent.getStringExtra("referSelectedBurger")
+        val getheure = intent.getStringExtra("referTimeDelivery")
+
+        val nom = findViewById<TextView>(R.id.nomClientConfirmation)
+        nom.text = getname
+        val prenom = findViewById<TextView>(R.id.prenomConfirmation)
+        prenom.text = getfirstname
+        val addresse = findViewById<TextView>(R.id.addressConfirmation)
+        addresse.text = getadresse
+        val numero = findViewById<TextView>(R.id.telephoneConfirmation)
+        numero.text = getnumero
+        val burger = findViewById<TextView>(R.id.burgerConfirmation)
+        burger.text = getburger
+        val heure = findViewById<TextView>(R.id.heureConfirmation)
+        heure.text = getheure
+
+
     }
 
 
--- app/src/main/java/fr/isen/lopez/celiandroidburger/MainActivity.kt
+++ app/src/main/java/fr/isen/lopez/celiandroidburger/MainActivity.kt
@@ -7,27 +7,53 @@ import android.content.SharedPreferences
 import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 import fr.isen.lopez.celiandroidburger.databinding.ActivityConfirmationBinding
 import fr.isen.lopez.celiandroidburger.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var tvTime : TextView
+    private lateinit var tvTime: TextView
     private lateinit var btnTimePicker: Button
-    private lateinit var nomClient : EditText
-
+    private lateinit var nomClient: EditText
+    private lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         val sharedPref = getSharedPreferences("myPref", MODE_PRIVATE)
+        spinner= findViewById(R.id.burger_spinner)
+        val burgers = arrayOf("Burger du chef","Cheese burger","Burger Montagnard","Burger Italien","Burger Végétarien")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, burgers)
+
+        spinner.adapter = adapter
+        var selectedItem =""
+
+        spinner.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+                selectedItem = burgers[p2]
+                Toast.makeText(this@MainActivity,"Votre choix : $selectedItem",Toast.LENGTH_LONG).show()
+
+
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+
+
+
+
+
+
+
         val editor = sharedPref.edit()
 
 
@@ -37,20 +63,14 @@ class MainActivity : AppCompatActivity() {
                 val nomClient = nomClient.text.toString()
                 val prenomClient = prenomClient.text.toString()
 
+
                 editor.apply {
-                    putString("nom_client",nomClient)
-                    putString("prenom_client",prenomClient)
+                    putString("nom_client", nomClient)
+                    putString("prenom_client", prenomClient)
                     apply()
                 }
-
-
             }
-
-
         }
-
-
-
 
         tvTime = findViewById(R.id.tvTime)
         btnTimePicker = findViewById(R.id.btnTimePicker)
@@ -62,31 +82,82 @@ class MainActivity : AppCompatActivity() {
             val startHour = currentTime.get(java.util.Calendar.HOUR_OF_DAY)
             val startMinute = currentTime.get(java.util.Calendar.MINUTE)
 
-            TimePickerDialog(this,TimePickerDialog.OnTimeSetListener { timePicker, i, i2 ->
+            TimePickerDialog(this, TimePickerDialog.OnTimeSetListener { timePicker, i, i2 ->
                 tvTime.setText("$i:$i2")
-            }, startHour,startMinute,false).show()
+            }, startHour, startMinute, false).show()
+
 
         }
+
+          fun changeActivityToConfirmation() {
+            startActivity(Intent(this, ConfirmationActivity::class.java))
+        }
+
+        val nom = findViewById<EditText>(R.id.nomClient)
+        val prenom = findViewById<EditText>(R.id.prenomClient)
+        val adresse = findViewById<EditText>(R.id.adressClient)
+        val numero = findViewById<EditText>(R.id.telephoneClient)
+        val heure = findViewById<TextView>(R.id.tvTime)
+        val burger = findViewById<Spinner>(R.id.burger_spinner)
+
+        val commander = findViewById<Button>(R.id.buttonValidation)
         binding.buttonValidation.setOnClickListener {
-            changeActivityToConfirmation()
+            commander.setOnClickListener { view: View ->
+
+                val txtnom = nom.text.toString()
+                val txtprenom = prenom.text.toString()
+                val txtadresse = adresse.text.toString()
+                val txtnumero = numero.text.toString()
+                val txtheure = heure.text.toString()
+
+
+
+                if (txtnom.trim().isEmpty() || txtprenom.trim().isEmpty() || txtadresse.trim()
+                        .isEmpty() || txtnumero.trim().isEmpty() || txtheure.trim().isEmpty()
+                ) {
+                    Toast.makeText(
+                        this,
+                        "les champs ne peuvent pas être vides !",
+                        Toast.LENGTH_LONG
+                    )
+                        .show()
+                } else {
+                    var ConfirmationActivity =
+                        Intent(this, ConfirmationActivity::class.java).apply {
+                            putExtra("referName", txtnom)
+                            putExtra("referAddress", txtadresse)
+                            putExtra("referPhone", txtnumero)
+                            putExtra("referTimeDelivery", txtheure)
+                            putExtra("referSelectedBurger", selectedItem)
+
+                        }
+                    val sharedPref = this?.getSharedPreferences(getString(R.string.sharedpref), Context.MODE_PRIVATE)
+                    //val sharedPref = getPreferences(Context.MODE_PRIVATE)
+                    with(sharedPref.edit()) {
+                        putString("saved_name", txtnom)
+                        putString("saved_address", txtadresse)
+                        putString("saved_phone", txtnumero)
+                        apply()
+                    }
+
+                    startActivity(ConfirmationActivity)
+
+                }
+
+            }
+
         }
 
-
-
-
-
-
     }
-
-
-    private fun changeActivityToConfirmation() {
-        startActivity(Intent(this, ConfirmationActivity::class.java))
-
-
-    }
-
-
-
-
 
 }
+
+
+
+
+
+
+
+
+
+
